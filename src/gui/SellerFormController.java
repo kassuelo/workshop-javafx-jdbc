623435227e01d1bd7ef6b5d7fcc58cfd483f0fa8
Kassuelo <kassuelo.mo@hotmail.com>
2021-01-28 21:08:33 -0300

    Department ComboBox

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -15,16 +15,24 @@ import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.util.Callback;
+import model.entities.Department;
 import model.entities.Seller;
 import model.exceptions.ValidationException;
+import model.services.DepartmentService;
 import model.services.SellerService;
 
 public class SellerFormController implements Initializable {
@@ -34,6 +42,8 @@ public class SellerFormController implements Initializable {
     private Seller entity;
 
     private SellerService service;
+
+    private DepartmentService departmentService;
 
     @FXML
     private TextField txtId;
@@ -49,6 +59,9 @@ public class SellerFormController implements Initializable {
 
     @FXML
     private TextField txtBaseSalary;
+
+    @FXML
+    private ComboBox<Department> comboBoxDepartment;
 
     @FXML
     private Label labelErrorName;
@@ -68,12 +81,15 @@ public class SellerFormController implements Initializable {
     @FXML
     private Button btCancel;
 
+    private ObservableList<Department> obsList;
+
     public void setSeller(Seller department) {
 	this.entity = department;
     }
 
-    public void setSellerService(SellerService service) {
+    public void setServices(SellerService service, DepartmentService departmentService) {
 	this.service = service;
+	this.departmentService = departmentService;
     }
 
     public void subscriberDataChangeListener(DataChangeListener listener) {
@@ -92,6 +108,20 @@ public class SellerFormController implements Initializable {
 	if (entity.getBirthDate() != null) {
 	    dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
 	}
+	if (entity.getDepartment() == null) {
+	    comboBoxDepartment.getSelectionModel().selectFirst();
+	} else {
+	    comboBoxDepartment.setValue(entity.getDepartment());
+	}
+    }
+
+    public void loadAssociatedObjects() {
+	if (departmentService == null) {
+	    throw new IllegalStateException("DepartmentService was null");
+	}
+	List<Department> list = departmentService.findAll();
+	obsList = FXCollections.observableArrayList(list);
+	comboBoxDepartment.setItems(obsList);
     }
 
     public void onBtSaveAction(ActionEvent event) {
@@ -101,6 +131,7 @@ public class SellerFormController implements Initializable {
 	if (service == null) {
 	    throw new IllegalStateException("Service was null");
 	}
+
 	try {
 	    entity = getFormData();
 	    service.saveOrUpdate(entity);
@@ -137,6 +168,7 @@ public class SellerFormController implements Initializable {
 	Constraints.setTextFieldDouble(txtBaseSalary);
 	Constraints.setTextFieldMaxLength(txtEmail, 60);
 	Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
+	initializeComboBoxDepartment();
     }
 
     private Seller getFormData() {
@@ -162,6 +194,18 @@ public class SellerFormController implements Initializable {
 	if (fields.contains("name")) {
 	    labelErrorName.setText(errors.get("name"));
 	}
+    }
+
+    public void initializeComboBoxDepartment() {
+	Callback<ListView<Department>, ListCell<Department>> factory = lv -> new ListCell<Department>() {
+	    @Override
+	    protected void updateItem(Department item, boolean empty) {
+		super.updateItem(item, empty);
+		setText(empty ? "" : item.getName());
+	    }
+	};
+	comboBoxDepartment.setCellFactory(factory);
+	comboBoxDepartment.setButtonCell(factory.call(null));
     }
 
 }
